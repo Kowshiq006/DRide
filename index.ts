b0364e9bd6b5c4cd8741e057b99d853bfe2564cc
Kowshiq <kowshiq@pop-os.localdomain>
2023-02-27 18:57:44 +0600

    second

--- index.ts
+++ index.ts
@@ -1,56 +1,53 @@
-import express, {Request, Response} from "express";
-import { PrismaClient } from '@prisma/client'
-
+import express, { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
 
 const app = express();
 app.use(express.json());
 
-
 const prisma = new PrismaClient();
 
-
-app.post("/", async(req: Request, res: Response) =>{
-    const{reqID, from, to, budget, status} = req.body;
-    const rideRequest = await prisma.rideRequest.create({
-        data: {
-            reqID : reqID,
-            from: from,
-            to: to,
-            budget: budget,
-            status: status
-        },
-    });
-    res.json(rideRequest);
+app.post("/", async (req: Request, res: Response) => {
+  const { reqID, from, to, budget, status } = req.body;
+  const rideRequest = await prisma.rideRequest.create({
+    data: {
+      reqID: reqID,
+      from: from,
+      to: to,
+      budget: budget,
+      status: status,
+    },
+  });
+  res.json(rideRequest);
 });
 
-app.get("/", async(req: Request, res: Response) =>{
-    const list = await prisma.rideRequest.findMany();
-    res.json(list);
+app.get("/", async (req: Request, res: Response) => {
+  const list = await prisma.rideRequest.findMany();
+  res.json(list);
 });
 
-app.put("/", async(req: Request, res: Response) =>{
-    const {reqID, budget} = req.body;
-    const newrideRequest = await prisma.rideRequest.update({
-        where : {
-            reqID: reqID,
-        },
-        data: {
-            budget: budget,
-        },
-    });
-    res.json(newrideRequest);
+app.put("/", async (req: Request, res: Response) => {
+  const { reqID, budget } = req.body;
+  const newrideRequest = await prisma.rideRequest.update({
+    where: {
+      reqID: reqID,
+    },
+    data: {
+      budget: budget,
+    },
+  });
+  res.json(newrideRequest);
 });
 
-app.delete("/:reqID", async (req: Request, res: Response) =>{
-    const reqID = req.params.reqID;
-    const deletedUser = await prisma.rideRequest.delete({
-        where: {
-            reqID: reqID,
-        },
-    });
-    res.json(deletedUser);
+app.delete("/:reqID", async (req: Request, res: Response) => {
+  const reqID = req.params.reqID;
+  const deletedUser = await prisma.rideRequest.delete({
+    where: {
+      reqID: reqID,
+    },
+  });
+  res.json(deletedUser);
 });
 
-app.listen(3001, ()=>{
-    console.log("Server Running on Port 3001");
-})
+app.listen(3001, () => {
+  console.log("Server Running on Port 3001");
+});
